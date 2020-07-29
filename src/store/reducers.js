169090ee760fe62eb1ab800d993708cecaa65dc5
milesswu <mileswu13@gmail.com>
2020-07-29 14:21:37 -0700

    Relocate userReducer to relevant location
    The HomePage will be where the user logs in, and so the userReducer should
    appropriately be placed in the duck for the HomePage.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,5 @@
 import { combineReducers } from "redux"
-
-import { UPDATE_USER } from "./actions"
-
-// Reducer for all user data
-const userReducer = (state = { userID: 1 }, action) => {
-	switch (action.type) {
-		case UPDATE_USER:
-			return action.payload
-		default:
-			return state
-	}
-}
+import userReducer from "../components/HomePage/duck/reducers"
 
 // Combine all app reducers; if any reducers exist nested in project directory (via ducks), import them here
 const rootReducer = combineReducers({

b976b51c57edcea83697283e3536eb4445857007
milesswu <mileswu13@gmail.com>
2020-07-29 11:53:08 -0700

    fix eslint test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from "react"
 
 function HomePage() {
 	return (
 		<>
 			<h1>Hello, World</h1>
 		</>
-	);
+	)
 }
 
-export default HomePage;
+export default HomePage

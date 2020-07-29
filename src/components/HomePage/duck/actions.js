3f1e01a3815c3ca7e1cb7f6936a79c87d0280359
milesswu <mileswu13@gmail.com>
2020-07-29 14:54:01 -0700

    comments for async action creators

--- src/components/HomePage/duck/actions.js
+++ src/components/HomePage/duck/actions.js
@@ -2,7 +2,12 @@
 export const UPDATE_USER = "UPDATE_USER"
 
 // ACTION CREATORS
-// All regular action creators should return an object of the form { type, payload } where payload contains the relevant information for the correspoding action
+/**
+ * All regular action creators should return an object of the form { type, payload }
+ * where payload contains the relevant information for the correspoding action
+ * All asynchronous action creators should return a function of the form function (dispatch) => {}
+ * where the dispatch function is used to dispatch an action object { type, payload }
+ */
 
 // updateUser action: used to update the current user information
 export const updateUser = (user) => {
@@ -11,3 +16,15 @@ export const updateUser = (user) => {
 		payload: user,
 	}
 }
+
+// example async action creator
+// export const sendRequest = (uid) => async (dispatch) => {
+// 	dispatch({ type: "REQUEST_SENT" })
+// 	try {
+// 		const info = await fetch(`/route/${uid}`)
+//    // notice we dispatch the action after we get a response from the async call
+// 		dispatch({ type: "REQUEST_FULFILLED", payload: info })
+// 	} catch (err) {
+// 		dispatch({ type: "REQUEST_DENIED", payload: err.message })
+// 	}
+// }
--- src/components/HomePage/duck/reducers.js
+++ src/components/HomePage/duck/reducers.js
@@ -1,7 +1,7 @@
 import { UPDATE_USER } from "./actions"
 
 // Reducer for all user data
-const userReducer = (state = { id: 1 }, action) => {
+const userReducer = (state = { uid: 1 }, action) => {
 	switch (action.type) {
 		case UPDATE_USER:
 			return action.payload

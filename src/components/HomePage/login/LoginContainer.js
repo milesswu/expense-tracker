52879c5820a29b72df858f9cc6e82c37d42e6cc0
milesswu <mileswu13@gmail.com>
2020-07-29 14:26:57 -0700

    comments for redux connection

--- src/components/HomePage/login/LoginContainer.js
+++ src/components/HomePage/login/LoginContainer.js
@@ -1,16 +1,21 @@
 import { connect } from "react-redux"
 import LoginComponent from "./LoginComponent"
 
+// import actions used in the component
 import { updateUser } from "../duck/actions"
 
+// Map data in the store to component props
 const mapStateToProps = (state) => ({
 	user: state.user,
 })
 
+// Map actions from the store to component props
 const mapDispatchToProps = (dispatch) => ({
 	updateUser: (user) => {
+		// dispatches the action
 		dispatch(updateUser(user))
 	},
 })
 
+// connect state and dispatch to redux stroe
 export default connect(mapStateToProps, mapDispatchToProps)(LoginComponent)

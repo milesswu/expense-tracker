2530d93fbf08053d8c61de0d284a335e4820144b
milesswu <mileswu13@gmail.com>
2020-07-29 11:18:53 -0700

    Add Material-UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Routes from "./pages/routes";
 import { BrowserRouter as Router } from "react-router-dom";
+import {
+	MuiThemeProvider,
+	createMuiTheme,
+	responsiveFontSizes,
+} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const overwrittenTheme = responsiveFontSizes(
+	createMuiTheme(), // TODO: Create custom theme object
+);
 
 function App() {
 	return (
-		<>
+		<MuiThemeProvider theme={overwrittenTheme}>
+			<CssBaseline />
 			<Router>
 				<Routes />
 			</Router>
-		</>
+		</MuiThemeProvider>
 	);
 }
 
